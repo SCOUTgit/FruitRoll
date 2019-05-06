cb6dfecfacc579fa4abf52ba9ef35e6e70e68741
SCOUTgit <k01095232802@gmail.com>
2019-05-06 21:56:32 +0900

    해상도 고정(close #15)

--- FruitRoll/Classes/AppDelegate.cpp
+++ FruitRoll/Classes/AppDelegate.cpp
@@ -96,7 +96,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0f / 60);
 
     // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::SHOW_ALL);
     auto frameSize = glview->getFrameSize();
     // if the frame's height is larger than the height of medium size.
     if (frameSize.height > mediumResolutionSize.height)
