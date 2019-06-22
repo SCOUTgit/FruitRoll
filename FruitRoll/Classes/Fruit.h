d421cd2dea9667281b3ca70bb2801158fab5eed1
An Byeong Gwan <mnsan1004@gmail.com>
2019-06-22 22:58:33 +0900

    Update Fruit.h

--- FruitRoll/Classes/Fruit.h
+++ FruitRoll/Classes/Fruit.h
@@ -9,7 +9,16 @@ class Fruit
 public:
 	Fruit(string type);
 	~Fruit();
-
+	
+	void MakeSprite();			// 애니메이트 생성
+	void Jump();				// 점프하기
+	void JumpEnd();				// 점프 끝내기
+	void Stop();				// 정지하기
+	void StopEnd();				// 정지 끝내기
+	void Rotate();				// 구르기
+	void PlayAnimation();		// 충돌 시 애니메이션 실행
+	
+public:
 	bool jumping;				// 점프하는 중인지 체크
 	bool stopping;				// 정지하는 중인지 체크
 	float fruitRadius;			// 과일의 반지름
@@ -19,12 +28,4 @@ public:
 	Label* stopLable;			// 스탑 레이블
 	Sprite* fruitImage;			// 과일의 이미지
 	Animation* fruitAnimation;	// 과일애니메이션
-
-	void MakeSprite();			// 애니메이트 생성
-	void Jump();				// 점프하기
-	void JumpEnd();				// 점프 끝내기
-	void Stop();				// 정지하기
-	void StopEnd();				// 정지 끝내기
-	void Rotate();				// 구르기
-	void PlayAnimation();		// 충돌 시 애니메이션 실행
 };
